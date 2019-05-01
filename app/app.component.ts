bb1b3acd5fbc383a27c1cd16378010fec16b9553
jorgepazp <paz.jorge1@gmail.com>
2019-05-01 13:26:52 -0400

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -20,19 +20,26 @@ changeSelected(current,id){
   document.getElementById("first-panel").classList.toggle("selected",id===1);
   document.getElementById("content-panel-1").classList.toggle("hide",id!==1);
   document.getElementById("icon-1").classList.toggle("sm-icon",id===1);
+  document.getElementById("icon-1").classList.toggle("rotate",id!==1);
+  document.getElementById("title-1").classList.toggle("rotate",id!==1);
 
   document.getElementById("second-panel").classList.toggle("selected",id===2);
   document.getElementById("content-panel-2").classList.toggle("hide",id!==2);
    document.getElementById("icon-2").classList.toggle("sm-icon",id===2);
+   document.getElementById("icon-2").classList.toggle("rotate",id!==2);
+   document.getElementById("title-2").classList.toggle("rotate",id!==2);
 
   document.getElementById("third-panel").classList.toggle("selected",id===3);
   document.getElementById("content-panel-3").classList.toggle("hide",id!==3);
-   document.getElementById("icon-3").classList.toggle("sm-icon",id===3);
-
+  document.getElementById("icon-3").classList.toggle("sm-icon",id===3);
+  document.getElementById("icon-3").classList.toggle("rotate",id!==3);
+  document.getElementById("title-3").classList.toggle("rotate",id!==3);
 
   document.getElementById("fourth-panel").classList.toggle("selected",id===4);
   document.getElementById("content-panel-4").classList.toggle("hide",id!==4);
  document.getElementById("icon-4").classList.toggle("sm-icon",id===4);
+ document.getElementById("icon-4").classList.toggle("rotate",id!==4);
+   document.getElementById("title-4").classList.toggle("rotate",id!==4);
   
    /* if(current === 1){
       document.getElementById("MyElement").classList.add('MyClass');
