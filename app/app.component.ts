2bb9492f04122a3f37ff5d45b3a179b54b17948d
jorgepazp <paz.jorge1@gmail.com>
2019-05-02 00:21:02 -0400

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,30 +10,40 @@ export class AppComponent  {
 //the following method receives the current
 //selected pane and does the proper changeSelected to the dom
 private flag:boolean = true;
+
+//specifies number of tabs
+private totalTabs:number = 4;
 changeSelected(current,id){
   if(this.flag){
     //We remove the initial configuration
     document.getElementById("first-panel").classList.remove("selected");
   }
 
+  for(var i=0;i<this.totalTabs;i++){
+    this.doChanges();
+    
+  }
   //the following lines adjust the width of the elements
   document.getElementById("first-panel").classList.toggle("selected",id===1);
   document.getElementById("content-panel-1").classList.toggle("hide",id!==1);
   document.getElementById("icon-1").classList.toggle("sm-icon",id===1);
   document.getElementById("icon-1").classList.toggle("rotate",id!==1);
   document.getElementById("title-1").classList.toggle("rotate",id!==1);
+  document.getElementById("title-1").classList.toggle("rt",id!==1);
 
   document.getElementById("second-panel").classList.toggle("selected",id===2);
   document.getElementById("content-panel-2").classList.toggle("hide",id!==2);
    document.getElementById("icon-2").classList.toggle("sm-icon",id===2);
    document.getElementById("icon-2").classList.toggle("rotate",id!==2);
    document.getElementById("title-2").classList.toggle("rotate",id!==2);
+  document.getElementById("title-1").classList.toggle("rt",id!==1);
 
   document.getElementById("third-panel").classList.toggle("selected",id===3);
   document.getElementById("content-panel-3").classList.toggle("hide",id!==3);
   document.getElementById("icon-3").classList.toggle("sm-icon",id===3);
   document.getElementById("icon-3").classList.toggle("rotate",id!==3);
   document.getElementById("title-3").classList.toggle("rotate",id!==3);
+  document.getElementById("title-1").classList.toggle("rt",id!==1);
 
   document.getElementById("fourth-panel").classList.toggle("selected",id===4);
   document.getElementById("content-panel-4").classList.toggle("hide",id!==4);
@@ -41,19 +51,10 @@ changeSelected(current,id){
  document.getElementById("icon-4").classList.toggle("rotate",id!==4);
    document.getElementById("title-4").classList.toggle("rotate",id!==4);
   
-   /* if(current === 1){
-      document.getElementById("MyElement").classList.add('MyClass');
-      document.getElementById("MyElement").classList.remove('MyClass');
-    }
-    if(current === 2){
+}
 
-    }
-    if(current === 3){
-
-    }
-    if(current === 4) {
-
-    }*/
+doChanges(){
+  
 }
   
 }
